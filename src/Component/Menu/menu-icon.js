606bcb121330f7e0634827d34f67516210095690
Dprof-Code <olawaleade15@gmail.com>
2023-07-03 11:58:14 -0500

    - implemented the Menu tab for both Mobile and Desktop - implemented the History slide for both Mobile and Desktop - Added the menu icon - Added the background image

---
+++ src/Component/Menu/menu-icon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuIcon from "../../Assets/menu-rounded.png"
+
+const MenuIcon = () => {
+    return (
+        <div>
+            <img src={MenuIcon} width="50px" height="50px" alt='Menu Bar icon' />
+        </div>
+    )
+}
+
+export default MenuIcon
--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -5,35 +5,60 @@ import "./Navbar.css"
 
 const Navbar = () => {
   return (
-    <div>
+    <header>
       <Link to="/">
-      <Logo/>
+        <Logo />
       </Link>
-      
-      <div>
-        <Link to="/about">
-        <p>About us</p>
-        </Link>
-        <Link to="/blog">
-        <p>Blog</p>
-        </Link>
-        <Link to="/sermons">
-        <p>Sermon</p>
-        </Link>
-        <Link to="/online_giving">
-        <p>Online Giving</p>
-        </Link>
-        <Link to="/excos">
-        <p>Meet Our Excos</p>
-        </Link>
-        
-       
-        
-       
-        
+
+      <div id="menu-icon" onClick={handleClick}>
+        <MenuIcon />
       </div>
-    </div>
+
+      <nav id="nav-bar">
+        <ul>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Sermon</a></li>
+          <li><a href="#">Online Giving</a></li>
+          <li><a className="highlighted" href="#">Meet our Excos</a></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
+
+console.log(nav);
+const handleClick = () => {
+  nav.classList.toggle("active");
+  console.log("clicked");
+}
+
+//const Navbar = () => {
+return (
+  <div>
+    <Link to="/">
+      <Logo />
+    </Link>
+
+    <div>
+      <Link to="/about">
+        <p>About us</p>
+      </Link>
+      <Link to="/blog">
+        <p>Blog</p>
+      </Link>
+      <Link to="/sermons">
+        <p>Sermon</p>
+      </Link>
+      <Link to="/online_giving">
+        <p>Online Giving</p>
+      </Link>
+      <Link to="/excos">
+        <p>Meet Our Excos</p>
+      </Link>
+    </div>
+  </div>
+)
+//}
 
 export default Navbar
