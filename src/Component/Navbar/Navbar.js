b3fb4bee7268f11a4e8f34c321f1a189dac4fb7e
Dprof-Code <olawaleade15@gmail.com>
2023-07-03 22:44:34 -0500

    - made changes to mobile screen fontsize - refactored the code - removed unused tags

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -5,91 +5,36 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='Navbar'>
-      <div className='Logo'>
-
     <header>
-
-      <Link to="/">
-        <Logo />
-      </Link>
-
+      <div className='Logo'>
+        <Link to="/">
+          <Logo />
+        </Link>
       </div>
-      
-      
+
       <div className='Navbar_link'>
         <Link to="/about">
-        <span className='Exco_para'>About us</span>
+          <span className='Exco_para'>About us</span>
         </Link>
         <Link to="/blog">
-        <span className='Exco_para'> Blog</span>
+          <span className='Exco_para'> Blog</span>
         </Link>
         <Link to="/sermons">
-        <span className='Exco_para'>Sermon</span>
+          <span className='Exco_para'>Sermon</span>
         </Link>
         <Link to="/online_giving">
-        <span className='Exco_para'>Online Giving</span>
+          <span className='Exco_para'>Online Giving</span>
         </Link>
         <Link to="/excos">
-        <span className='Exco_btn'>Meet Our Excos</span>
+          <span className='Exco_btn'>Meet Our Excos</span>
         </Link>
-  
-        
-       
-        
-       
-        
       </div>
 
       <div id="menu-icon" onClick={handleClick}>
         <MenuIcon />
       </div>
-
-      <nav id="nav-bar">
-        <ul>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Sermon</a></li>
-          <li><a href="#">Online Giving</a></li>
-          <li><a className="highlighted" href="#">Meet our Excos</a></li>
-        </ul>
-      </nav>
     </header>
   )
-}
-
-console.log(nav);
-const handleClick = () => {
-  nav.classList.toggle("active");
-  console.log("clicked");
-}
-
-//const Navbar = () => {
-return (
-  <div>
-    <Link to="/">
-      <Logo />
-    </Link>
-
-    <div>
-      <Link to="/about">
-        <p>About us</p>
-      </Link>
-      <Link to="/blog">
-        <p>Blog</p>
-      </Link>
-      <Link to="/sermons">
-        <p>Sermon</p>
-      </Link>
-      <Link to="/online_giving">
-        <p>Online Giving</p>
-      </Link>
-      <Link to="/excos">
-        <p>Meet Our Excos</p>
-      </Link>
-    </div>
-  </div>
-)
 }
 
 export default Navbar
