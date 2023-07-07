0c9b234d5cbf71d10b5c25c45915fd31ab27b9f8
Emmanuel Joseph <emmanueljoseph1320@gmail.com>
2023-07-07 02:36:50 +0100

    complete mission page

--- src/RouteFile.js
+++ src/RouteFile.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import { Routes, Route} from "react-router-dom";
-import Home from './Pages/Home/Home';
-import Blog from './Pages/Blog/Blog';
-import Excos from './Pages/Excos/Excos';
-import Sermons from './Pages/Sermons/Sermons';
-import AboutUs from './Pages/AboutUs/AboutUs';
-import OnlineGiving from './Pages/OnlineGiving/OnlineGiving';
-
-
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home/Home"
+import Blog from "./Pages/Blog/Blog"
+import Excos from "./Pages/Excos/Excos"
+import Sermons from "./Pages/Sermons/Sermons"
+import AboutUs from "./Pages/AboutUs/AboutUs"
+import OnlineGiving from "./Pages/OnlineGiving/OnlineGiving"
+import Mission from "./Component/Mission/Mission"
 
 const RouteFile = () => {
   return (
     <div>
-<Routes>
-          <Route path="/"  element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/excos" element={<Excos />} />
-          <Route path="/sermons" element={<Sermons />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/online_giving" element={<OnlineGiving />} />
-  </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/excos" element={<Excos />} />
+        <Route path="/sermons" element={<Sermons />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/online_giving" element={<OnlineGiving />} />
+      </Routes>
     </div>
   )
 }
