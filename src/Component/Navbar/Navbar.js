d96d109331d2b208e9ce9209f4fae67fb9fb86a6
olamiji-8 <olamijisotomi@gmail.com>
2023-07-09 13:46:21 +0100

    Working on the navbar menuicon

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,43 +1,119 @@
-import React from 'react'
-import Logo from '../Logo/Logo'
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React, { useState } from 'react';
+import Logo from '../Logo/Logo';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    background: `url('path/to/background-image.jpg') no-repeat center center fixed`,
+    backgroundSize: 'cover',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    display: 'none',
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+    },
+  },
+  title: {
+    flexGrow: 1,
+  },
+  navbarLink: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+      alignItems: 'center',
+      gap: 40,
+    },
+  },
+  menu: {
+    width: 250,
+  },
+}));
 
 const Navbar = () => {
-  return (
-    <div className='Navbar'>
-      <div className='Logo'>
-      <Link to="/">
-      <Logo/>
-      </Link>
-      </div>
-      
-      
-      <div className='Navbar_link'>
-        <Link to="/about">
-        <span className='Exco_para'>About us</span>
-        </Link>
-        <Link to="/blog">
-        <span className='Exco_para'> Blog</span>
-        </Link>
-        <Link to="/sermons">
-        <span className='Exco_para'>Sermon</span>
-        </Link>
-        <Link to="/online_giving">
-        <span className='Exco_para'>Online Giving</span>
-        </Link>
-        <Link to="/excos">
-        <span className='Exco_btn'>Meet Our Excos</span>
-        </Link>
-  
-        
-       
-        
-       
-        
-      </div>
-    </div>
-  )
-}
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const classes = useStyles();
 
-export default Navbar
+  const handleMenuOpen = () => {
+    setMenuOpen(true);
+  };
+
+  const handleMenuClose = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" color="transparent">
+        <Toolbar>
+          <div className={classes.title}>
+            <Link to="/">
+              <Logo />
+            </Link>
+          </div>
+          <div className={classes.navbarLink}>
+            <Link to="/about">
+              <span>About us</span>
+            </Link>
+            <Link to="/blog">
+              <span>Blog</span>
+            </Link>
+            <Link to="/sermons">
+              <span>Sermon</span>
+            </Link>
+            <Link to="/online_giving">
+              <span>Online Giving</span>
+            </Link>
+            <Link to="/excos">
+              <span>Meet Our Excos</span>
+            </Link>
+          </div>
+          <IconButton
+            edge="end"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="right" open={isMenuOpen} onClose={handleMenuClose}>
+        <div className={classes.menu} role="presentation">
+          <List>
+            <ListItem button component={Link} to="/about">
+              <ListItemText primary="About us" />
+            </ListItem>
+            <ListItem button component={Link} to="/blog">
+              <ListItemText primary="Blog" />
+            </ListItem>
+            <ListItem button component={Link} to="/sermons">
+              <ListItemText primary="Sermon" />
+            </ListItem>
+            <ListItem button component={Link} to="/online_giving">
+              <ListItemText primary="Online Giving" />
+            </ListItem>
+            <ListItem button component={Link} to="/excos">
+              <ListItemText primary="Meet Our Excos" />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/Component/Slide/Slide2/Slide2.js
+++ src/Component/Slide/Slide2/Slide2.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import Arrow from "../../../Assets/Arrow.svg"
+import React from 'react';
+import Arrow from "../../../Assets/Arrow.svg";
 import EastIcon from '@mui/icons-material/East';
-import "./Slide2.css"
+import "./Slide2.css";
 
 const Slide2 = () => {
   return (
     <div className='Slide2'>
-      <div className='Slide2_text'>
-      <p>A place where good things never cease!</p>
+      <div>
+        <span className='Slide2_text'>A place where good things never cease!</span><br/>
+        <div className='Slide2_flex'>
+          <span className='Slide2_body'>
+            The steadfast love of the LORD never ceases; his mercies never come to an end; they are new every morning;
+            <span className='Slide2_faithfulness'> great is your faithfulness. </span>
+          </span>
+          <span className='Slide2_arrow'><EastIcon fontSize='small' className='Arrow'/></span>
+        </div>
       </div>
-      <div className='Slide2_body'>
-        <p>The steadfast love of the LORD never ceases; his mercies never come to an end; they are new every morning; great is your faithfulness.</p>
-       <EastIcon className='Arrow'/>
-      </div>
-        
     </div>
-  )
-}
+  );
+};
 
 export default Slide2;
