3948b9e590430a76d6bbd2110b3596a8943b1f19
Emmanuel Joseph <emmanueljoseph1320@gmail.com>
2023-07-31 20:34:23 +0100

    mission statement numbered

--- src/Component/Mission/Mission.js
+++ src/Component/Mission/Mission.js
@@ -11,17 +11,36 @@ const Mission = () => {
         <div className="mission-content">
           <h1 className="header">MISSION STATEMENT</h1>
           <div className="mission-statement">
-            1. We shall give ourselves to in-depth study of God's word by the
-            help of the Holy Spirit and to prayer to understand our identity in
-            Christ as peculiar people zealous for good works. 2 Timothy 2:15, 2
-            Timothy 3:15-17, Acts 17:11, John 8:31-32, Acts 6:4, Ephesians
-            1:15-23. 2. We shall give ourselves to aggressive evangelism and
-            effective follow up with the help of the Holy Spirit to bring as
-            many souls who have gone astray and lost their relevance and
-            peculiarity back to Christ and ensuring that they abide. John 15:16,
-            Mark 16:15, Matthew 28:28-20, 1 Peter 1:12. 3. We shall work with
-            the Holy Spirit so that this life of peculiarity finds expression in
-            our daily lives. Romans 8:14, John 14:26.
+            <ol className="mission-ol">
+              <li className="mission-li">
+                We would give ourselves daily to the ministry of the word in
+                study and meditation for our complete furnishing, to know God’s
+                patterns for our living as Christians and we will live a life of
+                complete obedience to whatsoever God commands us to do. Joshua
+                1:8, James 1:25, Acts 6:4, Genesis 26:3-5, Deuteronomy 28:1-14,
+                2 Timothy 2:15; 3:16-17, 1 Timothy 4:13-16, Romans 6:16-20, John
+                15:14-15, 1 Peter 2:2.
+              </li>
+              <li className="mission-li">
+                We would as laborers in God’s vineyard be committed to
+                evangelism; in preaching the gospel and doing God’s works. John
+                15:14-16, 2 Corinthians 5:17-19, Matthew 5:14-16; 28:19-20, Mark
+                16:15-20, Acts 10:38, 1 Peter 1:12, 2 Timothy 4:2.{" "}
+              </li>
+              <li className="mission-li">
+                Holiness will be our lifestyle in thoughts, word and actions. 2
+                Timothy 2:19-21, 1 Peter 1;14-16, Ephesians 1:4, Philippians
+                4:8, 2 Peter 3:13-14, 1 Timothy 4;12, 1 Thessalonians 4:7,
+                Romans 6:16-20
+              </li>
+              <li className="mission-li">
+                We would live a consistent prayer and fasting life to be
+                spiritually-disciplined, so we would become that man that God
+                wants us to be on the earth. Genesis 1:26-28, 1 Thessalonians
+                5:17, Acts 2:42; 6:4, James 5:16-18, Colossians 4:2&12, 1
+                Corinthians 9:24-27, Romans 12:12, Luke 18:1.
+              </li>
+            </ol>
           </div>
         </div>
       </div>
