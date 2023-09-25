7d78f4269fc50c7aee32c79a6cfa778f80532972
olamiji-8 <olamijisotomi@gmail.com>
2023-09-25 01:31:33 +0100

    update

--- src/Pages/Sermons/Sermons.js
+++ src/Pages/Sermons/Sermons.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./Sermons.css"
+import sermonBg from '../../Assets/sermons.png'
+import Navbar from '../../Component/Navbar/Navbar'
 
 const Sermons = () => {
   return (
-    <div>Sermons</div>
+    <>
+      <Navbar />
+      <div className='sermon-container'>
+        <img src={sermonBg} alt="" />
+        <div className="sermon-text">
+          <div className="sermon-text-conatiner">
+            <div className="text-left">
+              <p className="date">June 28,2023</p>
+              <h1>Epignosis</h1>
+              <p className="preacher">Pst Sam Neye</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+
   )
 }
 
