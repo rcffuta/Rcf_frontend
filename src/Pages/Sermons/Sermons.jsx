f2fc09b27826f34adeb9f502085ec3ae237a67ae
unknown <olamademuyiwa@gmail.com>
2023-09-26 11:48:35 +0100

    second day of work

--- src/Pages/Sermons/Sermons.jsx
+++ src/Pages/Sermons/Sermons.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import "./Sermons.css"
 import sermonBg from '../../Assets/sermons.png'
 import Navbar from '../../Component/Navbar/Navbar'
-
+import HeadsetIcon from '@mui/icons-material/Headset';
+import CloudDownloadIcon from '@mui/icons-material/CloudDownloadOutlined';
+import EastIcon from '@mui/icons-material/East';
 const Sermons = () => {
   return (
     <>
@@ -10,11 +12,63 @@ const Sermons = () => {
       <div className='sermon-container'>
         <img src={sermonBg} alt="" />
         <div className="sermon-text">
-          <div className="sermon-text-conatiner">
+          <div className="sermon-text-container">
             <div className="text-left">
               <p className="date">June 28,2023</p>
               <h1>Epignosis</h1>
               <p className="preacher">Pst Sam Neye</p>
+              <div className="sermon-icon">
+                <HeadsetIcon/>
+                <CloudDownloadIcon />
+              </div>
+            </div>
+            <div className="text-right">
+              <span className="more-message">
+                <p>more sermons</p>
+                <p>view all</p>
+              </span>
+              <table>
+                <tr>
+                  <p className="date-posted">June 28, 2023</p>
+                  <div>
+                    <p className="message-title">Throughly furnished</p>
+                    <EastIcon/>
+                  </div>
+                  <hr />
+                </tr>
+                <tr>
+                  <p className="date-posted">June 28, 2023</p>
+                  <div>
+                    <p className="message-title">Throughly furnished</p>
+                    <EastIcon/>
+                  </div>
+                  <hr />
+                </tr>
+                <tr>
+                  <p className="date-posted">June 28, 2023</p>
+                  <div>
+                    <p className="message-title">Throughly furnished</p>
+                    <EastIcon/>
+                  </div>
+                  <hr />
+                </tr>
+                <tr>
+                  <p className="date-posted">June 28, 2023</p>
+                  <div>
+                    <p className="message-title">Throughly furnished</p>
+                    <EastIcon/>
+                  </div>
+                  <hr />
+                </tr>
+                <tr>
+                  <p className="date-posted">June 28, 2023</p>
+                  <div>
+                    <p className="message-title">Throughly furnished</p>
+                    <EastIcon/>
+                  </div>
+                  <hr />
+                </tr>
+              </table>
             </div>
           </div>
         </div>
