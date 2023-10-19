43cef2ac530069970c68ccd52e9e2ba44072702a
olamiji-8 <olamijisotomi@gmail.com>
2023-10-19 22:16:09 +0100

    update

--- src/Pages/Excos/Excos.js
+++ src/Pages/Excos/Excos.js
@@ -4,43 +4,57 @@ import Navbar from '../../Component/Navbar/Navbar';
 import backgroundImage from '../../Assets/Excos.png';
 
 const Excos = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const images = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg',
-    'image4.jpg',
-    'image5.jpg',
-    // Add more image URLs here
-  ];
+  const [startIndex, setStartIndex] = useState(0); // Index of the first visible image
+  const numVisibleBoxes = 5; // Number of visible boxes at a time
+  const totalImages = 50; // Total number of images
+  const boxHeightPercentage = 16; // Height of each box as a percentage of the screen height
+  const arrowHeightPercentage = 2; // Height of each arrow as a percentage of the screen height
+  const boxSpacingPercentage = 2; // Spacing between boxes as a percentage of the screen height
 
   const handleScroll = (direction) => {
-    if (direction === 'left') {
-      setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-    } else if (direction === 'right') {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    if (direction === 'up') {
+      setStartIndex((prevStartIndex) => Math.max(prevStartIndex - 1, 0));
+    } else if (direction === 'down') {
+      setStartIndex((prevStartIndex) => Math.min(prevStartIndex + 1, totalImages - numVisibleBoxes));
     }
   };
+
+  // Example image URLs for the 5 visible boxes
+  const visibleImages = [
+    'https://example.com/image1.jpg',
+    'https://example.com/image2.jpg',
+    'https://example.com/image3.jpg',
+    'https://example.com/image4.jpg',
+    'https://example.com/image5.jpg',
+  ];
 
   return (
     <div className="excos-container">
       <div className="background-image" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
       <Navbar />
       <div className="image-scroll">
-        <div className="left-arrow" onClick={() => handleScroll('left')}>
+        <div className="left-arrow" onClick={() => handleScroll('up')} style={{ height: `${arrowHeightPercentage}%` }}>
           &lt;
         </div>
-        <div className="image-container">
-          {images.map((image, index) => (
-            <img
+        <div className="image-container" style={{ height: `${100 - 2 * arrowHeightPercentage}%` }}>
+          {visibleImages.map((image, index) => (
+            <div
               key={index}
-              className={`image ${index === currentIndex ? 'active' : ''}`}
-              src={image}
-              alt=""
-            />
+              className="image-box"
+              style={{
+                height: `${boxHeightPercentage}%`,
+                marginBottom: `${boxSpacingPercentage}%`,
+              }}
+            >
+              <img
+                className={`image`}
+                src={image}
+                alt=""
+              />
+            </div>
           ))}
         </div>
-        <div className="right-arrow" onClick={() => handleScroll('right')}>
+        <div className="right-arrow" onClick={() => handleScroll('down')} style={{ height: `${arrowHeightPercentage}%` }}>
           &gt;
         </div>
       </div>
