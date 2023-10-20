47128bbd9f184b349f03708d59dc22f3e4230b9c
Damola Olutoke <olutokeezekiel18@gmail.com>
2023-10-20 08:12:42 +0100

    add pages

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,162 +1,340 @@
-import React, { useState } from 'react';
-import Logo from '../Logo/Logo';
-import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-} from '@material-ui/core';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
+// import React, { useState } from 'react';
+// import Logo from '../Logo/Logo';
+// import { Link } from 'react-router-dom';
+// import { makeStyles } from '@material-ui/core/styles';
+// import {
+//   AppBar,
+//   Toolbar,
+//   IconButton,
+//   Drawer,
+//   List,
+//   ListItem,
+//   ListItemText,
+// } from '@material-ui/core';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import CloseIcon from '@mui/icons-material/Close';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    background: `url('path/to/background-image.jpg') no-repeat center center fixed`,
-    backgroundSize: 'cover',
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    display: 'none',
-    [theme.breakpoints.down('sm')]: {
-      display: 'block',
-    },
-  },
-  title: {
-    flexGrow: 1,
-  },
-  navbarLink: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-      alignItems: 'center',
-      gap: 40,
-      color: 'white',
-    },
-    [theme.breakpoints.down('sm')]: {
-      marginTop: theme.spacing(10), // Adjust the value as needed
-    },
-    '& > a': {
-      textDecoration: 'none',
-      color: 'white',
-    },
-  },
-  excoLink: {
-    background: '#4DC8EB',
-    border: '2px solid #4DC8EB',
-    padding: '8px 16px',
-    borderRadius: '4px',
-    color: 'white',
-  },
-  menu: {
-    width: 250,
-  },
-  appBar: {
-    boxShadow: 'none',
-  },
-  menuIcon: {
-    color: 'white',
-  },
-  closeIcon: {
-    position: 'absolute',
-    top: theme.spacing(2),
-    right: theme.spacing(2),
-    color: 'black',
-    zIndex: 1,
-  },
-  closeIconContainer: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//     background: `url('path/to/background-image.jpg') no-repeat center center fixed`,
+//     backgroundSize: 'cover',
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//     display: 'none',
+//     [theme.breakpoints.down('sm')]: {
+//       display: 'block',
+//     },
+//   },
+//   title: {
+//     flexGrow: 1,
+//   },
+//   navbarLink: {
+//     display: 'none',
+//     [theme.breakpoints.up('md')]: {
+//       display: 'flex',
+//       alignItems: 'center',
+//       gap: 40,
+//       color: "dark",
+//     },
+//     [theme.breakpoints.down('sm')]: {
+//       marginTop: theme.spacing(10), // Adjust the value as needed
+//     },
+//     '& > a': {
+//       textDecoration: 'none',
+//       color: 'white',
+//     },
+//   },
+//   excoLink: {
+//     background: '#4DC8EB',
+//     border: '2px solid #4DC8EB',
+//     padding: '8px 16px',
+//     borderRadius: '4px',
+//     color: 'white',
+//   },
+//   menu: {
+//     width: 250,
+//   },
+//   appBar: {
+//     boxShadow: 'none',
+//   },
+//   menuIcon: {
+//     color: 'white',
+//   },
+//   closeIcon: {
+//     position: 'absolute',
+//     top: theme.spacing(2),
+//     right: theme.spacing(2),
+//     color: 'black',
+//     zIndex: 1,
+//   },
+//   closeIconContainer: {
+//     display: 'flex',
+//     justifyContent: 'flex-end',
+//   },
+// }));
 
-const Navbar = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const classes = useStyles();
+// const Navbar = () => {
+//   const [isMenuOpen, setMenuOpen] = useState(false);
+//   const classes = useStyles();
 
-  const handleMenuOpen = () => {
-    setMenuOpen(true);
-  };
+//   const handleMenuOpen = () => {
+//     setMenuOpen(true);
+//   };
 
-  const handleMenuClose = () => {
-    setMenuOpen(false);
-  };
+//   const handleMenuClose = () => {
+//     setMenuOpen(false);
+//   };
 
+//   return (
+//     <div className={classes.root}>
+//       <AppBar position="sticky" className={classes.appBar} color="transparent">
+//         <Toolbar>
+//           <div className={classes.title}>
+//             <Link to="/">
+//               <Logo />
+//             </Link>
+//           </div>
+//           <div className={classes.navbarLink}>
+//             <Link to="/about">
+//               <span>About us</span>
+//             </Link>
+//             <Link to="/blog">
+//               <span>Blog</span>
+//             </Link>
+//             <Link to="/sermons">
+//               <span>Sermon</span>
+//             </Link>
+//             <Link to="/online_giving">
+//               <span>Online Giving</span>
+//             </Link>
+//             <Link to="/excos" className={classes.excoLink}>
+//               <span>Meet Our Excos</span>
+//             </Link>
+//           </div>
+//           <IconButton
+//             edge="end"
+//             className={classes.menuButton}
+//             color="inherit"
+//             aria-label="menu"
+//             onClick={handleMenuOpen}
+//           >
+//             <MenuIcon className={classes.menuIcon} />
+//           </IconButton>
+//         </Toolbar>
+//       </AppBar>
+//       <Drawer anchor="right" open={isMenuOpen} onClose={handleMenuClose}>
+//         <div className={classes.closeIconContainer}>
+//           <IconButton
+//             className={classes.closeIcon}
+//             color="inherit"
+//             aria-label="close"
+//             onClick={handleMenuClose}
+//           >
+//             <CloseIcon />
+//           </IconButton>
+//         </div>
+//         <div className={classes.menu} role="presentation">
+//           <List>
+//             <ListItem button component={Link} to="/about">
+//               <ListItemText primary="About us" />
+//             </ListItem>
+//             <ListItem button component={Link} to="/blog">
+//               <ListItemText primary="Blog" />
+//             </ListItem>
+//             <ListItem button component={Link} to="/sermons">
+//               <ListItemText primary="Sermon" />
+//             </ListItem>
+//             <ListItem button component={Link} to="/online_giving">
+//               <ListItemText primary="Online Giving" />
+//             </ListItem>
+//             <ListItem button component={Link} to="/excos">
+//               <ListItemText primary="Meet Our Excos" />
+//             </ListItem>
+//           </List>
+//         </div>
+//       </Drawer>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar() {
   return (
-    <div className={classes.root}>
-      <AppBar position="fixed" className={classes.appBar} color="transparent">
-        <Toolbar>
-          <div className={classes.title}>
-            <Link to="/">
-              <Logo />
-            </Link>
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 absolute inset-x-0 top-0 z-50">
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-8 w-auto"
+                    src="https://res.cloudinary.com/dutcp8qkx/image/upload/v1697753724/rcf/rcf_logo_vgpnp5.svg"
+                    alt="logo"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 text-right sm:block">
+                  <div className="flex space-x-4">
+                    {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                    <a href="/about" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
+                      About Us
+                    </a>
+                    <a
+                      href="/blog"
+                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    >
+                      Blog
+                    </a>
+                    <a
+                      href="/sermons"
+                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    >
+                      Sermon
+                    </a>
+                    <a
+                      href="/online_giving"
+                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    >
+                      Online Giving
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex items-center">
+                  {/* <button
+                    type="button"
+                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  >
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">View notifications</span>
+                    <a
+                      href="/"
+                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    >
+                     Meet Our Excos
+                    </a>
+                  </button> */}
+                  <button
+                    type="button"
+                    className="relative inline-flex items-center gap-x-1.5 rounded-md bg-[#4dc8eb] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                  >
+                    <a href="/excos">
+                    Meet Our Excos
+                    </a>
+                    
+                  </button>
+                  
+                </div>
+              </div>
+              <div className="-mr-2 flex sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
           </div>
-          <div className={classes.navbarLink}>
-            <Link to="/about">
-              <span>About us</span>
-            </Link>
-            <Link to="/blog">
-              <span>Blog</span>
-            </Link>
-            <Link to="/sermons">
-              <span>Sermon</span>
-            </Link>
-            <Link to="/online_giving">
-              <span>Online Giving</span>
-            </Link>
-            <Link to="/excos" className={classes.excoLink}>
-              <span>Meet Our Excos</span>
-            </Link>
-          </div>
-          <IconButton
-            edge="end"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-            onClick={handleMenuOpen}
-          >
-            <MenuIcon className={classes.menuIcon} />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer anchor="right" open={isMenuOpen} onClose={handleMenuClose}>
-        <div className={classes.closeIconContainer}>
-          <IconButton
-            className={classes.closeIcon}
-            color="inherit"
-            aria-label="close"
-            onClick={handleMenuClose}
-          >
-            <CloseIcon />
-          </IconButton>
-        </div>
-        <div className={classes.menu} role="presentation">
-          <List>
-            <ListItem button component={Link} to="/about">
-              <ListItemText primary="About us" />
-            </ListItem>
-            <ListItem button component={Link} to="/blog">
-              <ListItemText primary="Blog" />
-            </ListItem>
-            <ListItem button component={Link} to="/sermons">
-              <ListItemText primary="Sermon" />
-            </ListItem>
-            <ListItem button component={Link} to="/online_giving">
-              <ListItemText primary="Online Giving" />
-            </ListItem>
-            <ListItem button component={Link} to="/excos">
-              <ListItemText primary="Meet Our Excos" />
-            </ListItem>
-          </List>
-        </div>
-      </Drawer>
-    </div>
-  );
-};
 
-export default Navbar;
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+              >
+                Dashboard
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              >
+                Team
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              >
+                Projects
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              >
+                Calendar
+              </Disclosure.Button>
+            </div>
+            <div className="border-t border-gray-700 pb-3 pt-4">
+              <div className="flex items-center px-5">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </div>
+                <div className="ml-3">
+                  <div className="text-base font-medium text-white">Tom Cook</div>
+                  <div className="text-sm font-medium text-gray-400">tom@example.com</div>
+                </div>
+                <button
+                  type="button"
+                  className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="mt-3 space-y-1 px-2">
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                >
+                  Your Profile
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                >
+                  Settings
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                >
+                  Sign out
+                </Disclosure.Button>
+              </div>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
+
--- src/Pages/OnlineGiving/OnlineGiving.js
+++ src/Pages/OnlineGiving/OnlineGiving.js
@@ -1,31 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './OnlineGiving.css';
+import ClipboardJS from 'clipboard';
 import Navbar from '../../Component/Navbar/Navbar';
 import Footerlogo from "../../Assets/Footerlogo.png"
 import OnlineGivingHeader from './onlineGivingHeader.js';
+import { ClipboardDocumentListIcon } from '@heroicons/react/20/solid'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const OnlineGiving = () => {
   const contentRef = useRef(null);
+  function handleCopy(textToCopy) {
+    const textArea = document.createElement('textarea');
+    textArea.value = textToCopy;
+  
+    document.body.appendChild(textArea);
 
-  const handleCopy = (text) => {
-    navigator.clipboard.writeText(text);
-    alert('Account number copied to clipboard!');
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const contentElement = contentRef.current;
-      const scrollPosition = contentElement.scrollTop;
-
-      contentElement.style.backgroundPositionY = `-${scrollPosition}px`;
-    };
-
-    const contentElement = contentRef.current;
-    contentElement.addEventListener('scroll', handleScroll);
-
-    return () => {
-      contentElement.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+    textArea.select();
+  
+    try {
+      document.execCommand('copy');
+      toast.success('Account number copied');
+    } catch (err) {
+      console.error('Unable to copy', err);
+      toast.error('Failed to copy account number');
+    }
+  
+    document.body.removeChild(textArea);
+  }
+  
 
   return (
     <div className="online-giving-container">
@@ -62,9 +65,37 @@ const OnlineGiving = () => {
             </div>
           </div>
         </div>
-        <div className="bank-details">
-          <h2>Bank Details</h2>
-          <div className="bank-info">
+
+        
+        <div className="relative mx-8 flex justify-center items-center max-w-2xl gap-x-32 gap-y-14 xl:max-w-none">
+          <div className='row '>  
+            <p className="font-display text-4xl font-medium tracking-tighter text-blue-900 sm:text-5xl">
+              Bank Details
+            </p>
+            <p className="font-display text-2xl font-medium tracking-tighter text-blue-900 sm:text-2xl py-4">
+              First Bank
+            </p>
+            <p>3041 232 323</p>
+            <p className="mt-4 text-lg tracking-tight text-blue-900">
+            The Redeemed Christian Fellowship, FUTA
+            </p>
+            <p className="mt-2 my-2 tracking-tight text-red-900 text-xs">
+            Kindly Indicate purpose of transaction in the narrations
+            </p>
+          </div>
+          <ToastContainer />
+          <div className='row justify-center items-center'>
+            <div className="copy-button" onClick={() => handleCopy('3041 232 323')}>
+              <ClipboardDocumentListIcon className="-ml-1.5 h-5 w-5 mx-1" aria-hidden="true" /> 
+                <button>
+                  Copy
+                </button>
+              </div>
+          </div>
+        
+
+          {/* <h2></h2> */}
+          {/* <div className="bank-info">
             <div className="bank-name">First Bank</div>
             <div className="account-number">3041 232 323</div>
             <div className="account-name">The Redeemed Christian Fellowship, FUTA</div>
@@ -72,13 +103,19 @@ const OnlineGiving = () => {
             <div className="copy-button" onClick={() => handleCopy('3041 232 323')}>
               Copy
             </div>
-          </div>
+          </div> */}
         </div>
+       
         <div className="online-giving-footer">
           <img className="online-giving-footer-image" src={Footerlogo} alt="" />
+          <div>
+          <p className='text-white'>A place where good things never cease</p>
+          </div>
         </div>
+        
       </div>
     </div>
+    
   );
 };
 
